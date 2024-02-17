5934a82e093534308bb4e1ac7d35af6ee1cba1e1
LauraaPaiva <lauraapaiva94@gmail.com>
2024-02-17 16:15:09 -0300

    Configuração dos campos do formulário de inserção do post

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,15 +1,54 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-post-form',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.css',
 })
 export class PostFormComponent {
+  postForm!: FormGroup;
+  submit() {
+    if (this.postForm.invalid) return;
+    console.log('Formulário enviado');
+  }
   @Input() btnText!: string;
+
+  ngOnInit(): void {
+    this.postForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.postForm.get('title')!;
+  }
+
+  get description() {
+    return this.postForm.get('description')!;
+  }
+
+  get image() {
+    return this.postForm.get('image')!;
+  }
 }
